3a9861a9abff675f66b7644ea4ee86d0f1ddb15e
ateBitDev <raul.santiago.aguirre@gmail.com>
2019-02-20 01:37:32 -0800

    Almost done

--- app.js
+++ app.js
@@ -12,7 +12,38 @@ app.set('view engine', 'pug');
 app.use('/', mainRoute);
 
 // catch 404 and forward to error handler
-
+app.use((req, res, next) =>
+{
+    const err = new Error('Not Found');
+    err.status = 404;
+    console.log(err.message);
+    console.log(err.status);
+    console.log(err.stack);
+    next(err)
+})
+//creats an error code for serverside issues such as missing data 
+app.use((req,res,next) =>
+{
+  const err = new Error('Error with the server');
+  err.status = 500;
+  console.log(err.message);
+  console.log(err.status);
+  console.log(err.stack);
+  next(err);
+});
+//renders the errors from error.pug temp
+app.use((err, req, res, next) => 
+{
+    res.status(err.status);
+    console.log(err.status)
+    if(err.status === 404){
+    res.render('page-not-found')
+    }
+    else{
+    console.log(err.status)
+    res.render('error', {error: err})
+    }
+})
 // error handler
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var Book = require("../models").Book;
+const bodyParser = require('body-parser');
 
+router.use(bodyParser.urlencoded({extended: false}));
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.redirect('/books');
@@ -9,24 +11,82 @@ router.get('/', function(req, res, next) {
 
 router.get('/books', function(req, res, next) {
   Book.findAll({order:[["title","ASC"]]}).then(function(books){
-    console.log(books)
     res.render('index', { title: 'Books', books : books });
+  }).catch(function(err){
+    res.sendStatus(500)
   })
 });
 
 router.get('/books/new', function(req, res, next) {
-  res.render('new-book', { title: 'Books'});
+  res.render('new-book', { title: 'New Book', book: Book.build()});
+});
+
+router.post('/books/new', function(req, res, next) {
+  Book.create(req.body).then(function(){
+    res.redirect("/");
+  }).catch(function(err){
+    if(err.name === "SequelizeValidationError"){
+      res.render("new-book",{
+        book: Book.build(req.body),
+        title: "New Book",
+        errors: err
+      });
+    }else{
+      throw err
+    }
+  }).catch(function(err){
+    res.sendStatus(500)
+  });
 });
 
 router.get('/books/:id', function(req, res, next) {
-  Book.findById(req.params.id).then(function(book){
-    console.log(book)
-    res.render('update-book', { title: 'Books', book : book });
+  Book.findByPk(req.params.id).then(function(book){
+    if(book){
+      res.render('update-book', { title: 'Update Book', book : book });
+    }else {
+      res.sendStatus(404)
+    }
+  }).catch(function(err){
+    res.sendStatus(500)
+  })
+});
+router.post('/books/:id', function(req, res, next) {
+  Book.findByPk(req.params.id).then(function(book){
+    if(book){
+      return book.update(req.body);
+    }else{
+      res.sendStatus(500)
+    }
+  }).then(function(){
+    res.redirect("/");
+  }).catch(function(err){
+    if(err.name === "SequelizeValidationError"){
+      var book = Book.build(req.body);
+      book.id = req.params.id;
+      res.render("update-book",{
+        book: book,
+        title: "Update Book",
+        errors: err
+      });
+    }else{
+      throw err
+    }}).catch(function(err){
+    res.sendStatus(500)
   })
 });
 
-router.get('/books/new', function(req, res, next) {
-    res.render('new-book', { title: 'Books', book : book });
+router.post('/books/:id/delete', function(req, res, next) {
+  Book.findByPk(req.params.id).then(function(book){
+    if(book){
+      return book.destroy();
+    }else{
+      res.sendStatus(404)
+    }
+  }).then(function(){
+    res.redirect("/");
+  }).catch(function(err){
+    res.sendStatus(500)
+  })
 });
 
 module.exports = router;
